bf07e189a06694a8bf13445e8c29f1472da8c59b
Stefan Wehr <wehr@factisresearch.com>
2013-08-09 15:46:05 +0200

    impressum and small fixes

---
+++ bob.js
@@ -0,0 +1,19 @@
+function make () {
+  var c = '@';
+  var r = '';
+  for (var i=0; i < arguments.length; i++) {
+    r = r + arguments[i];
+    var c;
+    switch (i) {
+    case 0: c = 'erenz@b'; break;
+    case 1: c = 'k'; break;
+    default: c = '.';
+    }
+    if (i != arguments.length - 1) {
+      r = r + c;
+    }
+  }
+  return r + '.de';
+}
+
+var e = make ('konf', 'ob', 'onf');
