6ae6f1c3adeb11d587d39a972fba7bac9b05223f
Mike Sperber <sperber@deinprogramm.de>
2015-08-19 11:54:49 +0200

    Multilingual 2015 index.

--- js/langdir.js
+++ js/langdir.js
@@ -2,14 +2,12 @@ var langCode = navigator.language || navigator.systemLanguage;
 var lang = langCode.toLowerCase(); lang = lang.substr(0,2);
 var dest = window.location.href;
 if (lang == "de") {
-  dest = "/de/index.html";
+  dest = dest.replace(/([a-zA-Z]*(\.html)?)$/, function r(x, p1) { return ["de/", p1].join(''); });
 } else {
-  dest = "/en/index.html";
+  dest = dest.replace(/([a-zA-Z]*(\.html)?)$/, function r(x, p1) { return ["en/", p1].join(''); });
 }
 if (window.location.replace) {
   window.location.replace(dest);
 } else {
   window.location=dest;
 }
-
-                    
