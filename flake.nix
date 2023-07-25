87fd0a306a575399de435be20196fc44aa331f10
Johannes Maier <johannes.maier@active-group.de>
2023-07-25 14:45:17 +0200

    Flakify

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Commands for development of the BOBKonf website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        jekyllFull = pkgs.jekyll.override {
+          # this way jekyll knows all the necessary plugins
+          withOptionalDependencies = true;
+        };
+      in {
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "build-blog";
+            src = pkgs.lib.cleanSource ./.;
+            buildInputs = [ jekyllFull ];
+            installPhase = ''
+              mkdir -p $out
+              cp -r . $out
+            '';
+          };
+          serve = pkgs.writeShellScriptBin "serve"
+            "${pkgs.lib.getExe jekyllFull} serve --watch --incremental";
+        };
+
+        apps.default =
+          flake-utils.lib.mkApp { drv = self.packages.${system}.serve; };
+
+        devShells.default =
+          pkgs.mkShell { nativeBuildInputs = [ jekyllFull ]; };
+
+        formatter = pkgs.nixfmt;
+      });
+}
